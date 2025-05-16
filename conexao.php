dde20e5c00db1bd25b29425c21f0d91b236b8592
kaikyGomezz <149087234+kaikyGomezz@users.noreply.github.com>
2025-05-16 18:48:13 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,13 +1,13 @@
 <?php
 $mysqli = new mysqli(
-  'switchyard.proxy.rlwy.net',
-  'root',
-  'UqRvkxHRiwvqDDoAEADLNXdmskmVaiES',
-  'railway',
- 40399
+  'switchyard.proxy.rlwy.net', // host
+  'root',                     // usuário
+  'UqRvkxHRiwvqDDoAEADLNXdmskmVaiES', // senha
+  'railway',                 // banco
+  40399                     // porta
 );
 
 if ($mysqli->connect_error) {
-  die('Erro na conexão: ' . $mysqli->connect_error);
+  die('Erro de conexão: ' . $mysqli->connect_error);
 }
 ?>
